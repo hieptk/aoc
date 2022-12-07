17589ebb28102a2150d2050a75d79674d8cb0279
hieptk <khiep94@gmail.com>
2022-12-07 18:18:38 +0100

    reformat

--- 04/dart.dart
+++ 04/dart.dart
@@ -4,8 +4,10 @@ void main(List<String> arguments) {
   File file = File('input.txt');
   int res = 0, res2 = 0;
   for (String line in file.readAsLinesSync()) {
-    List<int> arr = line.split(RegExp('[,-]')).map((e) => int.parse(e)).toList();
-    if ((arr[0] <= arr[2] && arr[1] >= arr[3]) || (arr[2] <= arr[0] && arr[3] >= arr[1])) {
+    List<int> arr =
+        line.split(RegExp('[,-]')).map((e) => int.parse(e)).toList();
+    if ((arr[0] <= arr[2] && arr[1] >= arr[3]) ||
+        (arr[2] <= arr[0] && arr[3] >= arr[1])) {
       res++;
     }
     if ((arr[0] > arr[3]) || (arr[1] < arr[2])) {
