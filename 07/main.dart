79753594f01ebf9cab1cd7d097093691f64cbdbd
hieptk <khiep94@gmail.com>
2022-12-07 17:34:58 +0100

    Refactor day 07

--- 07/main.dart
+++ 07/main.dart
@@ -1,15 +1,17 @@
 import 'dart:io';
 import 'dart:math';
 
+const int FS_SIZE = 70000000;
+const int REQUIRED_SIZE = 30000000;
+
 int res = 0;
 int res2 = 0;
-int fsSize = 70000000;
-int requiredSize = 30000000;
 
 class Node {
-  int size = 0, totalSize = 0;
+  int size = 0;
   Map<String, Node> children = {};
   Node? parent = null;
+  late int totalSize = children.values.fold<int>(size, (int sum, Node child) => sum + child.totalSize);
 }
 
 void addChild(Node p, String childName, {int size: 0}) {
@@ -22,63 +24,51 @@ void addChild(Node p, String childName, {int size: 0}) {
   p.children[childName] = child;
 }
 
-void calcTotalSize(Node p) {
-  p.totalSize = p.size;
-  for (String child in p.children.keys) {
-    calcTotalSize(p.children[child]!);
-    p.totalSize += p.children[child]!.totalSize;
-  }
+void part1(Node p) {
+  p.children.values.forEach((child) => part1(child));
   if (p.size == 0 && p.totalSize <= 100000) {
     res += p.totalSize;
   }
 }
 
-void findToDelete(Node p, int minSize) {
-  res2 = min(res2, p.totalSize);
-  for (String child in p.children.keys) {
-    if (p.children[child]!.totalSize >= minSize) {
-      findToDelete(p.children[child]!, minSize);
-    }
+void part2(Node p, int minSize) {
+  if (p.size > 0) {
+    return;
   }
+  res2 = min(res2, p.totalSize);
+  p.children.values.forEach((child) {
+    if (child.totalSize >= minSize) {
+      part2(child, minSize);
+    }
+  });
 }
 
 void main(List<String> arguments) {
   File file = File('input.txt');
-  List<String> lines = file.readAsLinesSync();
   Node root = Node();
   Node cur = root;
-  for (int i = 0; i < lines.length; ++i) {
-    List<String> tokens = lines[i].split(' ');
+  for (String line in file.readAsLinesSync()) {
+    List<String> tokens = line.split(' ');
     if (tokens[1] == 'cd') {
       if (tokens[2] == '..') {
-        if (cur.parent != null) {
-          cur = cur.parent!;
-        }
+        cur = cur.parent!;
       } else if (tokens[2] == '/') {
         cur = root;
       } else {
         addChild(cur, tokens[2]);
         cur = cur.children[tokens[2]]!;
       }
-    } else {
-      int j = i;
-      while (j + 1 < lines.length && !lines[j + 1].startsWith('\$')) {
-        j++;
-        List<String> tokens = lines[j].split(' ');
-        if (tokens[0] == 'dir') {
-          addChild(cur, tokens[1]);
-        } else {
+    } else if (tokens[1] != 'ls') {
+        if (tokens[0] != 'dir') { // don't need to care about dirs we don't go into
           addChild(cur, tokens[1], size: int.parse(tokens[0]));
         }
-      }
-      j = i;
     }
   }
 
-  calcTotalSize(root);
+  part1(root);
   print(res);
 
   res2 = root.totalSize;
-  findToDelete(root, requiredSize - (fsSize - root.totalSize));
+  part2(root, REQUIRED_SIZE - (FS_SIZE - root.totalSize));
   print(res2);
 }
